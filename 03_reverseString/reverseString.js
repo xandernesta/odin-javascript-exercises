b282bb414346878ac0b26e12a9daa2e19850e5ad
Alex N <xander.nesta@gmail.com>
2022-06-22 16:23:15 -0400

    complete exercises 3 & 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+let phraseArray = phrase.split('');
+let reversePhrase = (phraseArray.reverse()).join("");
+return reversePhrase;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    return array.filter(x => !args.includes(x));
+/*for my understanding:
+ return array.filter(function (x){
+    return !args.includes(x);
+ }
+ )
+this is separated by how I am used to reading, the actual answer has it condense to the arrow form.
+ */
+    
 };
 
 // Do not edit below this line
