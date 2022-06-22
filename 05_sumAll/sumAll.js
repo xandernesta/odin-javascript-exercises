8444fbb724864b6990fe97f509fd17e436973a29
Alex N <xander.nesta@gmail.com>
2022-06-22 17:00:13 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    if (Number.isInteger(x) &&  Number.isInteger(y) ){
+        if (x > 0 && y > 0){
+            if (x < y){
+                let i = y;
+                let sum = i;
+                while(i >= x){
+                sum = sum + i-1;
+                i--;
+            }
+            return sum;
+            }
+            else{
+                let i = x;
+                let sum = i;
+                while(i >= y){
+                    sum = sum + i-1;
+                    i--;
+                }
+            return sum;
+            }
+        }
+        else{
+            return 'ERROR';
+        }
+    }
+    else{
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
