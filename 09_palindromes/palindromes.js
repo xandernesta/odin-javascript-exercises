bdc489819906e19d670e14d0507bebda3c42a6db
Alex N <xander.nesta@gmail.com>
2022-06-27 17:31:21 -0400

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+let nopunc = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+let onlyletters = nopunc.split(' ').join('')
+let lettersarray= Array.from(onlyletters);
+let reversearray = lettersarray.reverse();
+if(reversearray.join('') === onlyletters){
+    return true;
+}
+else
+return false;
 };
 
 // Do not edit below this line
