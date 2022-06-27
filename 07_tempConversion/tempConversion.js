dda2cfffc0c9c6bdc1a1ef0b00488a85577587c1
Alex N <xander.nesta@gmail.com>
2022-06-27 13:45:19 -0400

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fnum) {
+  cresult = (fnum - 32) * (5/9)
+  roundedc = Number(cresult.toFixed(1));
+  return roundedc;
 };
 
-const ctof = function() {
-
+const ctof = function(cnum) {
+  fresult = cnum * (9/5) + 32
+  roundedf = Number(fresult.toFixed(1));
+  return roundedf;
 };
 
 // Do not edit below this line
