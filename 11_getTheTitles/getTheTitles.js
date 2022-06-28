8e0a5fd5cda354b483249931c52b1fa6837dfd72
Alex N <xander.nesta@gmail.com>
2022-06-28 15:09:32 -0400

    complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+let arrByTitle = array.map(book => book.title);
+return arrByTitle;
 };
 
 // Do not edit below this line
