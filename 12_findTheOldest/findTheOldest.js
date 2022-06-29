0d06bddbe03e0d3fce6b2628fe8cbb2af6a464a3
Alex N <xander.nesta@gmail.com>
2022-06-29 15:05:13 -0400

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(personArr) {
+let ageArr = personArr.map(
+    (element)=>{
+        if(!element.hasOwnProperty("yearOfDeath")){
+                element.yearOfDeath= new Date().getFullYear();
+        }
+        let age = Number(element.yearOfDeath) - Number(element.yearOfBirth);
+        return {...element, age: `${age}`};
+    });
 
+const oldest =  ageArr.reduce((prev,current) => (prev.age > current.age)? prev : current);
+return oldest;
 };
 
 // Do not edit below this line
